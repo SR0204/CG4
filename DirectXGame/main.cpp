5e527f0ef1ab3308bb5163c3bed5e5fd9981d44c
SR0204 <k023g0023@g.neec.ac.jp>
2025-04-16 14:52:15 +0900

    基礎完成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -42,13 +42,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// 解放処理
 
-	// エンジンの終了処理
-	KamataEngine::Finalize();
-
 	// ゲームシーンの解放
 	delete gameScene;
 	// nullptrの代入
 	gameScene = nullptr;
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
 
 	return 0;
 }
