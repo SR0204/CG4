78e50c7580e1e059413ca289a1376a097ed88afb
SR0204 <k023g0023@g.neec.ac.jp>
2025-06-24 10:17:12 +0900

    normalを逆に治した

--- DirectXGame/Model2/Model2.cpp
+++ DirectXGame/Model2/Model2.cpp
@@ -149,22 +149,22 @@ Model2* Model2::CreateSquare(int num) {
 	// 左下
 	vertices[0].pos = {-5.0f, -5.0f, 0.0f};
 	vertices[0].uv = {0.0f, 1.0f};
-	vertices[0].normal = {0.0f, 0.0f, 1.0f};
+	vertices[0].normal = {0.0f, 0.0f, -1.0f};
 
 	// 左上
 	vertices[1].pos = {-5.0f, 5.0f, 0.0f};
 	vertices[1].uv = {0.0f, 0.0f};
-	vertices[1].normal = {0.0f, 0.0f, 1.0f};
+	vertices[1].normal = {0.0f, 0.0f, -1.0f};
 
 	// 右下
 	vertices[2].pos = {5.0f * static_cast<float>(num), -5.0f, 0.0f};
 	vertices[2].uv = {1.0f * static_cast<float>(num), 1.0f};
-	vertices[2].normal = {0.0f, 0.0f, 1.0f};
+	vertices[2].normal = {0.0f, 0.0f, -1.0f};
 
 	// 右上
 	vertices[3].pos = {5.0f * static_cast<float>(num), 5.0f, 0.0f};
 	vertices[3].uv = {1.0f * static_cast<float>(num), 0.0f};
-	vertices[3].normal = {0.0f, 0.0f, 1.0f};
+	vertices[3].normal = {0.0f, 0.0f, -1.0f};
 
 	// インデックス
 	indeces[0] = 0;
