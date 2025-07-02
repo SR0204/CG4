2679a041e76c23cd1f98705766a346af64c66e4c
SR0204 <k023g0023@g.neec.ac.jp>
2025-07-02 14:28:56 +0900

    横につぶれていたのを修正した

--- DirectXGame/Model2/Model2.cpp
+++ DirectXGame/Model2/Model2.cpp
@@ -147,22 +147,22 @@ Model2* Model2::CreateSquare(int num) {
 	indeces.resize(kNumIndices);
 
 	// 左下
-	vertices[0].pos = {-5.0f, -5.0f, 0.0f};
+	vertices[0].pos = {-7.0f, -5.0f, 0.0f};
 	vertices[0].uv = {0.0f, 1.0f};
 	vertices[0].normal = {0.0f, 0.0f, -1.0f};
 
 	// 左上
-	vertices[1].pos = {-5.0f, 5.0f, 0.0f};
+	vertices[1].pos = {-7.0f, 5.0f, 0.0f};
 	vertices[1].uv = {0.0f, 0.0f};
 	vertices[1].normal = {0.0f, 0.0f, -1.0f};
 
 	// 右下
-	vertices[2].pos = {5.0f * static_cast<float>(num), -5.0f, 0.0f};
+	vertices[2].pos = {7.0f * static_cast<float>(num), -5.0f, 0.0f};
 	vertices[2].uv = {1.0f * static_cast<float>(num), 1.0f};
 	vertices[2].normal = {0.0f, 0.0f, -1.0f};
 
 	// 右上
-	vertices[3].pos = {5.0f * static_cast<float>(num), 5.0f, 0.0f};
+	vertices[3].pos = {7.0f * static_cast<float>(num), 5.0f, 0.0f};
 	vertices[3].uv = {1.0f * static_cast<float>(num), 0.0f};
 	vertices[3].normal = {0.0f, 0.0f, -1.0f};
 
